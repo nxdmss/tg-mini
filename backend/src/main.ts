8a8a41ac8f4c732ec3ec1f0bb7a9e76a536a6ae5
Umarchik2003 <nxdmss@gmail.com>
2026-06-06 13:46:54 +0300

    gav

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: [
+      'https://tgmini-blue.vercel.app',
+      'http://localhost:5173',
+    ],
+    credentials: true,
   });
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-    }),
-  );
-
-  const port = process.env.PORT || 3000;
-//
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
